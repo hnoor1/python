bb9e9cada44c7831f33e67f35bba0f263dbb6a7a
hnoor1 <hnoor@hkhk.edu.ee>
2025-10-09 14:43:28 +0300

    Add files via upload

--- h01.py
+++ h01.py
@@ -1,1 +1,52 @@
-print("Tere maailm!")
+import turtle
+
+#Hanna-Liina Nöör 09.10.2025
+
+#see on kujundite liigutamine
+turtle.penup()
+turtle.goto(-400,200)
+turtle.pendown()
+
+#see on kolmnurk!
+for i in range(3):
+    turtle.fd(200)
+    turtle.left(120)
+
+#see on ruudu tagasi liigutamine
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+
+#see on ruut
+for i in range(4): #tsükkel 4x
+    turtle.fd(150) #fd on forward
+    turtle.left(90)
+
+#liigume
+turtle.penup()
+turtle.goto(-100,-300)
+turtle.pendown()
+
+#see on minu nime esitäht - Liina
+turtle.fd(200)
+turtle.right(90)
+turtle.fd(50)
+turtle.left(90)
+turtle.bk(250)
+turtle.left(90)
+turtle.fd(300)
+turtle.right(90)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(250)
+
+
+
+
+
+
+
+
+
+
+turtle.done()
