ddb5040036fa4edbc3ecb4cffae9a0566745f08f
hnoor1 <hnoor@hkhk.edu.ee>
2025-10-27 00:03:35 +0200

    Teine kodune ülesanne
    Teine kodune ülesanne

---
+++ kodune_ül02.py
@@ -0,0 +1,62 @@
+import turtle
+
+# turtle.forward(100) #ühe kolmnurga tegemine
+# turtle.right(60)
+# turtle.backward(100)
+# turtle.right(60)
+# turtle.forward(100)
+turtle.speed(50)
+
+for kolmnurk in range(3):#teeme kolmnurga
+    turtle.forward(100) 
+    turtle.left(120)
+    
+for kolmnurk in range(3):
+    turtle.backward(100)
+    turtle.left(120)
+    
+for kolmnurk in range(3):
+    turtle.left(120)
+    turtle.backward(100)
+turtle.forward(100)
+
+for kolmnurk in range(3):
+    turtle.left(120)
+    turtle.backward(100)
+
+turtle.left(60)
+turtle.backward(100)
+
+for kolmnurk in range(3):
+    turtle.left(120)
+    turtle.backward(100)
+
+for kolmnurk in range(3):
+    turtle.backward(100)
+    turtle.left(120)
+    
+for kolmnurk in range(3):
+    turtle.backward(100)
+    turtle.right(120)
+
+turtle.left(60)
+
+for kolmnurk in range(3):
+    turtle.backward(100)
+    turtle.left(120)
+
+turtle.backward(100)
+turtle.right(120)
+
+for kolmnurk in range(3):
+    turtle.forward(100) 
+    turtle.left(120)
+    
+turtle.backward(100)
+
+for kolmnurk in range(3):
+    turtle.forward(100)
+    turtle.right(120)
+    
+    
+turtle.done()
