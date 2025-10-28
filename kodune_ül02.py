973379c126a50026f670fdf6f9a0e2c362a72bfe
hnoor1 <hnoor@hkhk.edu.ee>
2025-10-29 01:25:50 +0200

    II kodune ülesanne - kujund nr 12
    II kodune ülesanne - kujund nr 12

--- kodune_ül02.py
+++ kodune_ül02.py
@@ -7,21 +7,21 @@ import turtle
 turtle.speed(0)
 #Kujund koosneb 12 kolmnurgast, selle sees on ühtlasi ka kuusnurk
 
-#teeme kuus kolmnurka, mis moodustavad tähe kujundi, tähe sisse moodustub tühi kuusnurk
+#Teeme kuus kolmnurka, mis moodustavad tähe kujundi, tähe sisse moodustub tühi kuusnurk
 for kolmnnurk in range(6):
-    #ühe kolmnurga tegemine
+    #Ühe kolmnurga tegemine
     for kolmnurk in range(3):
         turtle.forward(100)
         turtle.left(120)
-    #kui üks kolmnurk on tehtud, saab teha kuus samasugust kolmnurka
+    #Kui üks kolmnurk on tehtud, saab teha kuus samasugust kolmnurka
     turtle.forward(100)
     turtle.right(60)
 
-#liigume tekkinud kuusnurga sisse ülejäänud kolmnurkade joonistamiseks
+#Liigume tekkinud kuusnurga sisse ülejäänud kolmnurkade joonistamiseks
 turtle.right(60)
 turtle.forward(100)
 
-#teeme kuusnurga sisse kuus kolmnurka
+#Teeme kuusnurga sisse kuus kolmnurka
 for kolmnurk in range(6):
     turtle.forward(100)
     turtle.right(120)
