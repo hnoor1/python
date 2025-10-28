f244e83ca67bd40b57f344ac9d8a68e364bc95b2
hnoor1 <hnoor@hkhk.edu.ee>
2025-10-28 22:45:31 +0200

    2 kodune ülesanne - tsüklid, kujund nr 12
    2 kodune ülesanne - tsüklid, kujund nr 12

--- kodune_ül02.py
+++ kodune_ül02.py
@@ -1,13 +1,19 @@
+#Kodune ülesanne nr 2 - kujund nr 12 (https://www.metshein.com/wp-content/uploads/2023/10/python-turtle-KT-scaled.jpg)
+#Hanna-Liina Nöör 28.10.2025
+
 import turtle
 
-# turtle.forward(100) #ühe kolmnurga tegemine
-# turtle.right(60)
-# turtle.backward(100)
-# turtle.right(60)
-# turtle.forward(100)
-turtle.speed(50)
+# turtle.forward(100) #ühe kolmnurga tegemine ilma tsüklita
+    # turtle.right(60)
+    # turtle.backward(100)
+    # turtle.right(60)
+    # turtle.forward(100)
 
-for kolmnurk in range(3):#teeme kolmnurga
+turtle.speed(0)
+
+#Kujundi nr 12 saamiseks tuleb teha kolmnurgad.Selleks kasutan tsükleid.
+
+for kolmnurk in range(3):
     turtle.forward(100) 
     turtle.left(120)
     
